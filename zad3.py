3795c5c04ecee9517502cf3cc4dec07b448a9a71
mikhail <mikhail@spbserv.RTITS.RU>
2022-03-25 12:46:30 +0000

    test1

--- zad3.py
+++ zad3.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
-
+import sys
 write_file=set()
 import os
-dir = os.path.abspath(os.curdir)
-bash_command = ["dir", "git status"]
-print ('&&' .join(bash_command))
+filename =  input ('Вв :')
+
+print (filename)
+
+if filename == '':
+	dir = os.path.abspath(os.curdir)
+	print (dir)
+	bash_command = ["cd "+dir, "git status"]
+else:
+	dir = os.path.abspath(filename)
+	print ('2' + dir)
+	bash_command = ["cd "+dir, "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 is_change = False
 for result in result_os.split('\n'):
